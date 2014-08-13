1d51fcc2793f56987cd399d62def80a6ab96fb9f
AeroSteveO <thesteveo64@gmail.com>
2014-08-13 16:35:07 -0400

    working pretty well now

--- src/Wheatley/EnglishSayings.java
+++ src/Wheatley/EnglishSayings.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 import org.pircbotx.Colors;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
@@ -26,13 +27,27 @@ public class EnglishSayings extends ListenerAdapter {
         String message = Colors.removeFormattingAndColors(event.getMessage());
         
         if (message.equalsIgnoreCase("!saying")){
-//            if (sayings == null){
-//                sayings = getSayings();
-//            }
+            switch((int) (Math.random()*3+1)) {
+                case 1:
+                    event.getBot().sendIRC().message(event.getChannel().getName(),sayings.get((int) (Math.random()*sayings.size()-1)));
+                    break;
+                case 2:
+                    event.getBot().sendIRC().message(event.getChannel().getName(),randHyphenatedSaying());
+                    break;
+                case 3:
+                    event.getBot().sendIRC().message(event.getChannel().getName(),randCommaSeparatedSaying());
+                    break;
+            } 
+        }
+        if (message.equalsIgnoreCase("!saying -")){ //hyphenated sayings only
+            event.getBot().sendIRC().message(event.getChannel().getName(),randHyphenatedSaying());
+        }
+        if (message.equalsIgnoreCase("!saying ,")){ //comma separated sayings only
+            event.getBot().sendIRC().message(event.getChannel().getName(),randCommaSeparatedSaying());
+        }
+        if (message.equalsIgnoreCase("!saying .")){ //purist sayings only
             event.getBot().sendIRC().message(event.getChannel().getName(),sayings.get((int) (Math.random()*sayings.size()-1)));
         }
-        
-        
     }
     private ArrayList<String> getSayings() {
         try{
@@ -48,4 +63,38 @@ public class EnglishSayings extends ListenerAdapter {
             return null;
         }
     }
+    private String randHyphenatedSaying() {
+        String saying = "";
+        ArrayList<String> start = new ArrayList<String>();
+        ArrayList<String> end = new ArrayList<String>();
+        
+        for (int i=0;i<sayings.size();i++){
+            if (Pattern.matches("[a-zA-Z]+\\-[a-z,A-Z]+", sayings.get(i))){
+                start.add(sayings.get(i).split("-")[0]);
+                end.add(sayings.get(i).split("-")[1]);
+//                System.out.print(sayings.get(i));
+            }
+        }
+        return (start.get((int) (Math.random()*start.size()-1))+"-"+end.get((int) (Math.random()*end.size()-1)));
+    }
+        private String randCommaSeparatedSaying() {
+        String saying = "";
+        ArrayList<String> start = new ArrayList<String>();
+        ArrayList<String> end = new ArrayList<String>();
+        
+        for (int i=0;i<sayings.size();i++){
+            if (Pattern.matches("[a-zA-Z\\s]+\\,[a-zA-Z\\s]+", sayings.get(i))){
+                try {
+                start.add(sayings.get(i).split(",")[0]);
+                end.add(sayings.get(i).split(",")[1]);
+//                System.out.println(sayings.get(i));
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return (start.get((int) (Math.random()*start.size()-1))+","+end.get((int) (Math.random()*end.size()-1)));
+    }
+    
 }
