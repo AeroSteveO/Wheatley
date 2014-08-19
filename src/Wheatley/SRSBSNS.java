76e5089e647d95047e6132c71cbe68a987f24709
AeroSteveO <thesteveo64@gmail.com>
2014-08-18 21:37:09 -0400

    added !christmas and !summon

--- src/Wheatley/SRSBSNS.java
+++ src/Wheatley/SRSBSNS.java
@@ -9,6 +9,8 @@ package Wheatley;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import org.pircbotx.Colors;
 import org.pircbotx.hooks.ListenerAdapter;
@@ -18,7 +20,7 @@ import org.pircbotx.hooks.events.MessageEvent;
  *
  * @author Steve-O
  *      Original Bot: SRSBSNS by: i dunno who
- * 
+ *
  * Activate Command with:
  *      !lasturl
  *          pulls up the last url seen in the current channel
@@ -28,6 +30,13 @@ import org.pircbotx.hooks.events.MessageEvent;
  */
 public class SRSBSNS extends ListenerAdapter {
     List<String> UrlHistory = new ArrayList<>();
+    
+//    <srsbsns> srsbsns: srsbsns supports: !srsbsns (responds: wat), 
+//    !whodef (who defined), !whatis (definition), !explain (definition), 
+//    !randomdef (random definition), !rt (rotten tomatoes movie rating), 
+//    !udict (urban dictionary), !imdb (imdb movie search), !metacritic (metacritic.com rating), 
+//    !lasturl (analyzes the last url posted), !summon (person), !christmas (countdown to Christmas)
+    
     
     @Override
     public void onMessage(MessageEvent event) throws Exception {
@@ -55,6 +64,42 @@ public class SRSBSNS extends ListenerAdapter {
                     event.getBot().sendIRC().action(currentChan,"Second to last URL: "+Global.Channels.getChan(currentChan).getSecondLastUrl());
                 else
                     event.getBot().sendIRC().action(currentChan,"Currently less than 2 URLs found");
+            }
+            if(message.equalsIgnoreCase("!christmas")) {
+                GregorianCalendar now = new GregorianCalendar();
+                GregorianCalendar christmas = new GregorianCalendar();
+                christmas.set(Calendar.MONTH, 11);
+                christmas.set(Calendar.DAY_OF_MONTH, 25);
+                if(christmas.before(now)) {
+                    christmas.add(Calendar.YEAR, 1);
+                }
+                else if(christmas.equals(now)) {
+                    event.respond("It's Christmas!");
+                }
+                christmas.set(Calendar.HOUR_OF_DAY, 0);
+                christmas.set(Calendar.MINUTE, 0);
+                christmas.set(Calendar.SECOND, 0);
+                long diff = (christmas.getTimeInMillis() - now.getTimeInMillis())/1000;
+                long days = (diff/86400);
+                diff %= 86400;
+                long hours = diff/3600;
+                diff %= 3600;
+                long minutes = diff/60;
+                diff %=60;
+                long seconds = diff;
+                event.respond(String.format("There are %d days, %d hours, %d minutes, and %d seconds until Christmas", days, hours, minutes, seconds));
+            }
+            //String message = event.getMessage().trim();
+            if(message.matches("!summon\\s+[^\\s]+")) {
+                String target = message.split("\\s+")[1];
+                if(event.getBot().getUserChannelDao().getAllUsers().contains(event.getBot().getUserChannelDao().getUser(target))) {
+                    //If the user is in the same channel as the summon
+                    event.getBot().sendIRC().notice(event.getUser().getNick(), Colors.BOLD+"!summon "+Colors.NORMAL+target+" has been PMed");
+                    event.getBot().sendIRC().message(event.getBot().getUserChannelDao().getUser(target).getNick(), Colors.BOLD+"!summon "+Colors.NORMAL+"you have been summoned by "+event.getUser().getNick());
+                }
+                else {
+                    event.getBot().sendIRC().notice(event.getUser().getNick(), Colors.BOLD+"!summon "+Colors.NORMAL+"user not in channel");
+                }
             }
         }
     }
