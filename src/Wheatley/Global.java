1181cfc5d557a4b1c09d933e9fc3dd1e09581a81
AeroSteveO <thesteveo64@gmail.com>
2014-10-04 19:35:01 -0400

    added comments about what everything is for

--- src/Wheatley/Global.java
+++ src/Wheatley/Global.java
@@ -13,8 +13,16 @@ import org.pircbotx.PircBotX;
 /**
  *
  * @author Steve-O
- * YES GASP I KNOW GLOBAL VARIABLES ARE REALLY NICE SOMETIMES
- *
+ * botOwner      - User with all powers over the bot, and ability to shut the bot down
+ * mainNick      - The intended nickname for the bot, not necessarily the current nickname
+ * nickPass      - The nickServ password for the bot
+ * botAdmins     - Administrators of the bot, ability to use some commands
+ * commandPrefix - The character that signals the bot that a command is being sent
+ * reconnect     - Boolean to activate the aggressive server reconnect loop
+ * bot           - Current PircBotX bot object
+ * channels      - Array of settings for each channel the bot is in
+ * activeGame    - Array of games currently active in channels the bot is in
+ * 
  */
 public class Global {
     public static String botOwner = new String(); //Updated in the Main .java file from Setings.XML
@@ -23,6 +31,6 @@ public class Global {
     public static boolean reconnect = true;
     public static PircBotX bot;
     public static ChannelArray channels = new ChannelArray();
-    public static String commandPrefix = "!";       // Not implemented yet in other functions
-    static GameArray activeGame = new GameArray();  // To be implemented in games
+    public static String commandPrefix = "!";              // Not implemented yet in other functions
+    public static GameArray activeGame = new GameArray();  // To be implemented in games
 }
