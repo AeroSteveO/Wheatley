ede3511770aaa260741f18b83e48d256cf5cf06c
AeroSteveO <thesteveo64@gmail.com>
2014-11-08 11:08:05 -0500

    cleaned and yet another blackjack card update

--- src/Objects/PlayingCard.java
+++ src/Objects/PlayingCard.java
@@ -1,15 +1,15 @@
-/**
- * Models a playing card, with possible values of Ace through King
- * in one of the four suits.  May also be a Joker.
- * <p>
- * Cards are not modifiable once created.
- * <p>
- * When suits need to be sorted, bridge suit value is used, producing the
- * following order: (highest) SPADES, HEARTS, DIAMONDS, CLUBS (lowest).
- *
- * @author Zach Tomaszewski
- * @version 15 Feb 2005
- */
+ /**
+  * Models a playing card, with possible values of Ace through King
+  * in one of the four suits.  May also be a Joker.
+  * <p>
+  * Cards are not modifiable once created.
+  * <p>
+  * When suits need to be sorted, bridge suit value is used, producing the
+  * following order: (highest) SPADES, HEARTS, DIAMONDS, CLUBS (lowest).
+  *
+  * @author Zach Tomaszewski
+  * @version 15 Feb 2005
+  */
 
 package Objects;
 
@@ -21,11 +21,6 @@ public class PlayingCard implements Comparable {
     * Notes/Todo:
     * --There are normally two jokers, distinguishable from each other.
     *   Right now, a joker is a joker.
-    * --Java supports Unicode, and unicode has suit characters:
-    *   \U+2660 to \U+2667, or #9824 to #9831.
-    *   If I can get these to print anywhere, add a static variable
-    *   controlling toString's output from plain ascii text.
-    *   [char literal: '\\uCODE', w/o first \]
     * --New decks are created A-K.  Perhaps should be 2-A,
     *   so AS is bottom face card?
     *
@@ -36,25 +31,21 @@ public class PlayingCard implements Comparable {
     * --a comparator that sorts suit and then value (for sorting hands)
     */
     
-//face card values
-    
-    
+    //face card values
     public static final int ACE = 1;
     public static final int JACK = 11;
     public static final int QUEEN = 12;
     public static final int KING = 13;
     public static final int JOKER = 0;
-//suits, in bridge rank order, highest to lowest
+    //suits, in bridge rank order, highest to lowest
     public static final int SPADES = 4;
     public static final int HEARTS = 3;
     public static final int DIAMONDS = 2;
     public static final int CLUBS = 1;
-    /** a null suit for Jokers or other non-suit cards */
+    // a null suit for Jokers or other non-suit cards
     public static final int NONE = 0;
     
-    
-//instance variables
-    private int value; //sometimes call pips
+    private int value;
     private int suit;
     
     
@@ -78,39 +69,39 @@ public class PlayingCard implements Comparable {
             this.suit = NONE;
         }
     }
-       /**
-    * Returns the suit of this card.
-    * @returns the suit, which is one of the constants Card.SPADES, 
-    * Card.HEARTS, Card.DIAMONDS, Card.CLUBS, or Card.JOKER
-    */
-   public int getSuit() {
-      return suit;
-   }
-   
-   /**
-    * Returns the value of this card.
-    * @return the value, which is one of the numbers 1 through 13, inclusive for
-    * a regular card, and which can be any value for a Joker.
-    */
-   public int getValue() {
-      return value;
-   }
-   
-   /**
-    * Returns a String representation of the card's suit.
-    * @return one of the strings "Spades", "Hearts", "Diamonds", "Clubs"
-    * or "Joker".
-    */
-   public String getSuitAsString() {
-      switch ( suit ) {
-      case SPADES:   return "Spades";
-      case HEARTS:   return "Hearts";
-      case DIAMONDS: return "Diamonds";
-      case CLUBS:    return "Clubs";
-      default:       return "Joker";
-      }
-   }
-
+    /**
+     * Returns the suit of this card.
+     * @returns the suit, which is one of the constants Card.SPADES,
+     * Card.HEARTS, Card.DIAMONDS, Card.CLUBS, or Card.JOKER
+     */
+    public int getSuit() {
+        return suit;
+    }
+    
+    /**
+     * Returns the value of this card.
+     * @return the value, which is one of the numbers 1 through 13, inclusive for
+     * a regular card, and which can be any value for a Joker.
+     */
+    public int getValue() {
+        return value;
+    }
+    
+    /**
+     * Returns a String representation of the card's suit.
+     * @return one of the strings "Spades", "Hearts", "Diamonds", "Clubs"
+     * or "Joker".
+     */
+    public String getSuitAsString() {
+        switch ( suit ) {
+            case SPADES:   return "Spades";
+            case HEARTS:   return "Hearts";
+            case DIAMONDS: return "Diamonds";
+            case CLUBS:    return "Clubs";
+            default:       return "Joker";
+        }
+    }
+    
     
 //METHODS (instance) --------
     
@@ -238,9 +229,9 @@ public class PlayingCard implements Comparable {
                 break;
         }
         return card;
-    }//<Asamarui> ♤, ♥, ♢, ♧
-    //<Asamarui> i.e these ♠♥♦♣
-        public String toColoredString() {
+    }
+    //  ♠♥♦♣
+    public String toColoredString() {
         String card = "";
         switch (this.value) {
             case JOKER:
@@ -284,29 +275,6 @@ public class PlayingCard implements Comparable {
     
     
 // INNER CLASSES ------------
-    
-    /*
-    * ICS211 Note: The comparators below are an example of inner classes.
-    * You don't see them very often,
-    * and I recommend you don't use them unless you have a good reason to.
-    * Normally it's for parts or components of a class so intimately
-    * connected to that class you want to make it part of that class.
-    * (I originally wrote Deck as an inner class, but decided it could be
-    * used for non-PlayingCard purposes.  Even if that's not true, a deck
-    * seems to be a collection of PlayingCards, rather than some inherent
-    * feature or function of a PlayingCard.  So I made Deck a separate class.
-    * However, a comparator that sorts Playing Cards seems so closely tied
-    * to Playing Cards, I thought to make it an inner class.
-    *
-    * The equals methods for all my Comparators (which determines whether
-    * two comparators are the same) was going to be the same code for all four
-    * Comparators.  Rather than write this four times, I made an abstract
-    * super class so I could write this method and leave compare (the other
-    * method from the Compartor interface) to be implemented later.
-    * (Another option would have been to just extend my first Collator,
-    * overriding the compare method in each case.)
-    */
-    
     
     /**
      * The superclass of all PlayingCard.Comparators.
@@ -427,41 +395,4 @@ public class PlayingCard implements Comparable {
             }
         }
     }
-    
-    
-    
-//MAIN (for PlayingCard) --------------
-    
-    /**
-     * Runs a few sample tests on printing cards and creating Comparators,
-     * printing results to the screen.
-     */
-//    public static void main(String[] args) {
-//        
-//        System.out.print("A few sample PlayingCards: ");
-//        System.out.print(new PlayingCard(4, HEARTS) + " ");
-//        System.out.print(new PlayingCard(2, CLUBS) + " ");
-//        System.out.print(new PlayingCard(10, SPADES) + " ");
-//        System.out.print(new PlayingCard(JACK, DIAMONDS) + " ");
-//        System.out.print(new PlayingCard(JOKER, NONE) + "\n");
-//        System.out.println();
-//        
-//        PlayingCard card1 = new PlayingCard(KING, CLUBS);
-//        PlayingCard card2 = new PlayingCard(KING, SPADES);
-//        System.out.print(card1 + " equals " + card2 + " [false]: ");
-//        System.out.println(card1.equals(card2));
-//        System.out.print(card1 + " equalsIgnoreSuit " + card2 + " [true]: ");
-//        System.out.println(card1.equalsIgnoreSuit(card2));
-//        System.out.println();
-//        
-//        java.util.Comparator comp = new PlayingCard.Comparator();
-//        System.out.print("A Comparator equals a Comparator [true]: ");
-//        System.out.println( comp.equals(new PlayingCard.Comparator()) );
-//        System.out.print("A Comparator equals a ComparatorIgnoreSuit [false]: ");
-//        java.util.Comparator comp2 = new PlayingCard.ComparatorIgnoreSuit();
-//        System.out.println( comp.equals(comp2) );
-//        
-//    }
-    
-    
 }//end class PlayingCard
