40e9613bff90ece3c66dc48eb73f56c9d60d20b7
AeroSteveO <thesteveo64@gmail.com>
2014-11-20 19:14:55 -0500

    slightly more robust bomb game

--- src/Wheatley/GameBomb.java
+++ src/Wheatley/GameBomb.java
@@ -30,7 +30,7 @@ import org.pircbotx.hooks.events.MessageEvent;
  * - Linked Classes
  *    Global
  *    GameControl
- * 
+ *
  * Activate Command with:
  *      !bomb
  *          Gives the user a bomb, the user has to pick the correctly colored
@@ -43,59 +43,65 @@ public class GameBomb extends ListenerAdapter {
     String blockedChan = "#dtella";
     ArrayList<String> colorls = null;
     int prize = 70; // $
+    int loss = 50;
     
     @Override
     public void onMessage(MessageEvent event) throws FileNotFoundException, InterruptedException {
         String message = Colors.removeFormattingAndColors(event.getMessage());
         
         if (message.equalsIgnoreCase("!bomb")&&!Global.channels.areGamesBlocked(event.getChannel().getName())){
-            if (colorls == null) {
-                colorls = getColorList();
-            }
-            String player = event.getUser().getNick();
-            List<String> colours = new ArrayList<>();
-            String colorlist = "";
-            for (int i=0;i<5;i++){
-                colours.add(colorls.get((int) (Math.random()*colorls.size()-1)).toLowerCase());
-            }
             
-//            if (colorlist.equals("")){
-            for (int i=0; i<colours.size()-1;i++){
-                colorlist = colorlist + colours.get(i) + ", ";
+            if (GameControl.scores.getScore(event.getUser().getNick())<loss){
+                event.getBot().sendIRC().notice(event.getUser().getNick(),"You don't have enough money to afford the potential loss in this game");
             }
-            colorlist = colorlist + colours.get(colours.size()-1);
+            else{
+                if (colorls == null) {
+                    colorls = getColorList();
+                }
+                String player = event.getUser().getNick();
+                List<String> colours = new ArrayList<>();
+                String colorlist = "";
+                for (int i=0;i<5;i++){
+                    colours.add(colorls.get((int) (Math.random()*colorls.size()-1)).toLowerCase());
+                }
+                
+//            if (colorlist.equals("")){
+                for (int i=0; i<colours.size()-1;i++){
+                    colorlist = colorlist + colours.get(i) + ", ";
+                }
+                colorlist = colorlist + colours.get(colours.size()-1);
 //            }
-            int key = (int) (Math.random()*100000+1);
+                int key = (int) (Math.random()*100000+1);
 //            DateTime dt = new DateTime();
 //            DateTime end = dt.plusSeconds(time);
-            String solution = colours.get((int) (Math.random()*colours.size()-1));
-            event.respond("You recieved the bomb. You have " + time + " seconds to defuse it by cutting the right cable." + Colors.BOLD + " Choose your destiny:" + Colors.NORMAL);
-            event.getBot().sendIRC().message(event.getChannel().getName(),"Wire colors include: " + colorlist);
-            TimedWaitForQueue queue = new TimedWaitForQueue(event, time, key);
-            while (true){
-                MessageEvent CurrentEvent = queue.waitFor(MessageEvent.class);
+                String solution = colours.get((int) (Math.random()*colours.size()-1));
+                event.respond("You recieved the bomb. You have " + time + " seconds to defuse it by cutting the right cable." + Colors.BOLD + " Choose your destiny:" + Colors.NORMAL);
+                event.getBot().sendIRC().message(event.getChannel().getName(),"Wire colors include: " + colorlist);
+                TimedWaitForQueue queue = new TimedWaitForQueue(event, time, key);
+                while (true){
+                    MessageEvent CurrentEvent = queue.waitFor(MessageEvent.class);
 //                dt = new DateTime();
-                if (CurrentEvent.getMessage().equalsIgnoreCase(Integer.toString(key))){
-                    int moneyLoss = 50;
-                    event.getBot().sendIRC().message(event.getChannel().getName(),"the bomb explodes in front of " + player + ". Seems like you did not even notice the big beeping suitcase. You lose $"+moneyLoss);
-                    colours.clear();
-                    GameControl.scores.subtractScore(player, moneyLoss);
-                    GameControl.scores.addScore(event.getBot().getNick(), moneyLoss);
-                    queue.close();
-                }
-                else if (CurrentEvent.getMessage().equalsIgnoreCase(solution)&&CurrentEvent.getUser().getNick().equalsIgnoreCase(player)&&CurrentEvent.getChannel().getName().equalsIgnoreCase(event.getChannel().getName())){
-                    event.getBot().sendIRC().message(event.getChannel().getName(), player + " defused the bomb. Seems like he was wise enough to buy a defuse kit. You win $"+prize );
-                    colours.clear();
-                    GameControl.scores.addScore(player,prize);
-                    queue.close();
-                }
-                else if (!CurrentEvent.getMessage().equalsIgnoreCase(solution)&&CurrentEvent.getUser().getNick().equalsIgnoreCase(player)&&CurrentEvent.getChannel().getName().equalsIgnoreCase(event.getChannel().getName())){
-                    int moneyLoss = 20;
-                    event.getBot().sendIRC().message(event.getChannel().getName(),"The bomb explodes in " + player + "'s hands. You lost your life and - even worse - $"+moneyLoss+". The right color would have been "+Colors.BOLD+Colors.RED+solution);
-                    colours.clear();
-                    GameControl.scores.subtractScore(player, moneyLoss);
-                    GameControl.scores.addScore(event.getBot().getNick(), moneyLoss);
-                    queue.close();
+                    if (CurrentEvent.getMessage().equalsIgnoreCase(Integer.toString(key))){
+                        event.getBot().sendIRC().message(event.getChannel().getName(),"the bomb explodes in front of " + player + ". Seems like you did not even notice the big beeping suitcase. You lose $"+loss);
+                        colours.clear();
+                        GameControl.scores.subtractScore(player, loss);
+                        GameControl.scores.addScore(event.getBot().getNick(), loss);
+                        queue.close();
+                    }
+                    else if (CurrentEvent.getMessage().equalsIgnoreCase(solution)&&CurrentEvent.getUser().getNick().equalsIgnoreCase(player)&&CurrentEvent.getChannel().getName().equalsIgnoreCase(event.getChannel().getName())){
+                        event.getBot().sendIRC().message(event.getChannel().getName(), player + " defused the bomb. Seems like he was wise enough to buy a defuse kit. You win $"+prize );
+                        colours.clear();
+                        GameControl.scores.addScore(player,prize);
+                        queue.close();
+                    }
+                    else if (!CurrentEvent.getMessage().equalsIgnoreCase(solution)&&CurrentEvent.getUser().getNick().equalsIgnoreCase(player)&&CurrentEvent.getChannel().getName().equalsIgnoreCase(event.getChannel().getName())){
+                        int moneyLoss = 20;
+                        event.getBot().sendIRC().message(event.getChannel().getName(),"The bomb explodes in " + player + "'s hands. You lost your life and - even worse - $"+moneyLoss+". The right color would have been "+Colors.BOLD+Colors.RED+solution);
+                        colours.clear();
+                        GameControl.scores.subtractScore(player, moneyLoss);
+                        GameControl.scores.addScore(event.getBot().getNick(), moneyLoss);
+                        queue.close();
+                    }
                 }
             }
         }
