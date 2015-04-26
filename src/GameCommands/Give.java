4ab2394a7aefd9727f07ff29cb064be39f2b5d86
AeroSteveO <thesteveo64@gmail.com>
2015-04-25 22:28:54 -0400

    basic layout of some other game commands

---
+++ src/GameCommands/Give.java
@@ -0,0 +1,35 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+
+package GameCommands;
+
+import Objects.Command;
+import Wheatley.Global;
+import java.util.ArrayList;
+import org.pircbotx.hooks.Event;
+
+/**
+ *
+ * @author Stephen
+ */
+public class Give implements Command{
+    @Override
+    public void processCommand(Event event){
+        
+    }
+    @Override
+    public ArrayList<String> commandTerms(){
+        ArrayList<String> a = new ArrayList<>();
+        a.add("give");
+        return a;
+        
+    }
+    @Override
+    public boolean isCommand(String toCheck){
+        return false;
+    }
+    
+}
