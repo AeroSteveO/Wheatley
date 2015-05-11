c1042d84d0899dd7063578576a54f0f4838992e1
AeroSteveO <thesteveo64@gmail.com>
2015-05-10 21:12:45 -0400

    added second method for getting a string from a URL

--- src/Utils/TextUtils.java
+++ src/Utils/TextUtils.java
@@ -13,6 +13,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -42,6 +43,8 @@ import java.util.Scanner;
  *
  */
 public class TextUtils {
+    private final String USER_AGENT = "Mozilla/5.0";
+    
     public static String loadText(String filename) throws FileNotFoundException, IOException{
         File file =new File(filename);
         
@@ -110,6 +113,37 @@ public class TextUtils {
                 reader.close();
         }
     }
+    
+    public String readUrlUsingGet(String url) throws Exception {
+        
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        
+        con.setRequestMethod("GET");
+        
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        
+//        con.setRequestProperty("X-Mashape-Key", key);
+        
+        int responseCode = con.getResponseCode();
+//        System.out.println("\nSending 'GET' request to URL : " + url);
+//        System.out.println("Response Code : " + responseCode);
+        
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        
+//        System.out.println(response.toString());
+        return response.toString();
+    }
+    
     public static boolean addToDocIfUnique(String filename, String addition){
         try{
             File file;
