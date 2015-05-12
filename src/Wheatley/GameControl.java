546bfcdd0c11c27626dd2bedd6ad8e16d5fbc1ab
AeroSteveO <thesteveo64@gmail.com>
2015-05-12 19:34:53 -0400

    shifting around comment lines

--- src/Wheatley/GameControl.java
+++ src/Wheatley/GameControl.java
@@ -1,3 +1,9 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+
 package Wheatley;
 
 import Objects.GameList;
@@ -14,12 +20,6 @@ import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.JoinEvent;
 import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.events.UserListEvent;
-
-/*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
 
 /**
  *
