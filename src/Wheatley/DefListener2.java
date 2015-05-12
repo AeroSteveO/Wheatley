2506fa301052b0f6ddb59f2b32763f03a6350364
AeroSteveO <thesteveo64@gmail.com>
2015-05-11 22:06:59 -0400

    cleaning

--- src/Wheatley/DefListener2.java
+++ src/Wheatley/DefListener2.java
@@ -8,11 +8,8 @@ package Wheatley;
 
 import Objects.Definitions;
 import Utils.IRCUtils;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
