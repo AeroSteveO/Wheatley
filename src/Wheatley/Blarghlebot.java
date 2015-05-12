2ab7abcc9835d730e989ef5076416e4532c1a439
AeroSteveO <thesteveo64@gmail.com>
2015-05-12 19:35:48 -0400

    added new !BF and !BF [nick] commands, which take the last line spoken (or spoken by the input nick) and reverses it before sending it back out

--- src/Wheatley/Blarghlebot.java
+++ src/Wheatley/Blarghlebot.java
@@ -67,7 +67,7 @@ public class Blarghlebot extends ListenerAdapter {
         if (message.toLowerCase().startsWith("sw/")||((message.toLowerCase().startsWith("s/")||message.toLowerCase().startsWith("sed/"))&&!event.getBot().getUserChannelDao().getChannels(event.getBot().getUserChannelDao().getUser("BlarghleBot")).contains(event.getChannel()))){
             
             if (message.endsWith("/"))
-                    message+="poop";
+                message+="poop";
             
             String[] findNreplace = Colors.removeFormattingAndColors(message.toLowerCase()).split("/");
             
@@ -78,6 +78,58 @@ public class Blarghlebot extends ListenerAdapter {
                 addToLog(channel, reply);
             }
         }
+        
+        if (message.startsWith(Global.commandPrefix)&&!message.matches("([ ]{0,}"+Global.commandPrefix+"{1,}[ ]{0,}){1,}")){
+            
+            String command = message.split(Global.commandPrefix)[1];
+            String[] cmdSplit = command.split(" ");
+            
+            
+            if (cmdSplit[0].equalsIgnoreCase("bf")){
+                if (cmdSplit.length==2){
+                    String nick = cmdSplit[1];
+                    
+                    int i=log.get(channel).size()-2;
+                    boolean found = false;
+                    String line = new String();
+                    
+                    while (!found&&i>=0){
+                        if(log.get(event.getChannel().getName()).get(i).get(0).replaceAll("(<|>)", "").equalsIgnoreCase(nick)){
+                            found = true;
+                            nick = log.get(event.getChannel().getName()).get(i).get(0);
+                            line = log.get(event.getChannel().getName()).get(i).get(1);
+                            System.out.println(nick + " " + line);
+                        }
+                        i--;
+                    }
+                    if (!found){
+                        event.getBot().sendIRC().notice(event.getUser().getNick(), "!BF nick not found in log");
+                    }
+                    else{
+                        String[] words = line.split(" ");
+                        String newLine = "";
+                        for (int c=words.length-1;c>=0;c--){
+                            newLine+=words[c]+" ";
+                        }
+                        event.getBot().sendIRC().message(event.getChannel().getName(), nick+" "+newLine);
+                    }
+                }
+                else if (cmdSplit.length>2){
+                    event.getBot().sendIRC().notice(event.getUser().getNick(), "!BF may take a nickname as input, or no input at all");
+                }
+                else{
+                    String nick = log.get(event.getChannel().getName()).get(log.get(event.getChannel().getName()).size()-2).get(0);
+                    String line = log.get(event.getChannel().getName()).get(log.get(event.getChannel().getName()).size()-2).get(1);
+                    String[] words = line.split(" ");
+                    String newLine = "";
+                    for (int i=words.length-1;i>=0;i--){
+                        newLine+=words[i]+" ";
+                    }
+                    event.getBot().sendIRC().message(event.getChannel().getName(), nick+" "+newLine);
+                }
+            }
+        }
+        
         if ((message.toLowerCase().startsWith(Global.mainNick.toLowerCase()+", ")&&!message.toLowerCase().startsWith(Global.mainNick.toLowerCase()+", why")&&!message.toLowerCase().startsWith(Global.mainNick.toLowerCase()+", what do you think of")&&message.endsWith("?"))){
             //Messages from : http://en.wikipedia.org/wiki/Magic_8-Ball
             List<String> magic = new ArrayList<>();
@@ -105,7 +157,7 @@ public class Blarghlebot extends ListenerAdapter {
         }
         
         if (!event.getBot().getUserChannelDao().getChannels(event.getBot().getUserChannelDao().getUser("BlarghleBot")).contains(event.getChannel())) {
-                        
+            
             if (message.equalsIgnoreCase("!users")||message.equalsIgnoreCase("!dtellausers")){
                 int totalUsers = 0;
                 int dtellaChanUsers = 0;
