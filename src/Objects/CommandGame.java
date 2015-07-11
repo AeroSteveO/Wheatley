326199397570ddde30ace85a5d2ddf11b1d67950
AeroSteveO <thesteveo64@gmail.com>
2015-07-11 00:10:14 -0400

    working on making the game listener more functional with games running under it, new interface command game similar to the command interface but with game specific methods

---
+++ src/Objects/CommandGame.java
@@ -0,0 +1,70 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+
+package Objects;
+
+import java.util.ArrayList;
+import org.pircbotx.hooks.events.MessageEvent;
+
+/**
+ *
+ * @author Stephen
+ *
+ * Requirements:
+ * - APIs
+ *    N/A
+ * - Custom Objects
+ *    N/A
+ * - Linked Classes
+ *    N/A
+ *
+ */
+public interface CommandGame  {
+    
+    /**
+     *
+     * @return boolean true if the object implementing the interface is a game
+     * and false if it is a general command
+     * 
+     * -Games would include stuff like hangman
+     * -Non-games would include stuff like money, cheat, and give
+     */
+    boolean isGame();
+    
+    /**
+     *
+     * @return boolean true if the game is a multi-line, "Long" game, and false
+     * if the game is a single line "Short" game
+     * 
+     * -Single line games are those that only require the bot to output a single
+     *  line (Think lotto, omgword, and bomb)
+     * -Long games may require constant interaction between the user and the bot
+     *  (Think hangman, and mastermind)
+     */
+    boolean isShortGame();
+    
+    /**
+     *
+     * @param event the event that triggered the game/command to be run, allows
+     * the command to use the full data from the event.
+     */
+    public void processCommand(MessageEvent event);
+    
+    /**
+     *
+     * @return ArrayList<String> of terms that will activate the command
+     */
+    public ArrayList<String> commandTerms();
+    
+    /**
+     *
+     * @param toCheck
+     * @return boolean true if the string is an activating command, false if the
+     * string is not to activate the game/command
+     */
+    public boolean isCommand(String toCheck);
+    
+}
--- src/Wheatley/GameListener.java
+++ src/Wheatley/GameListener.java
@@ -6,9 +6,12 @@
 
 package Wheatley;
 
+import GameCommands.GameBombCMD;
 import GameCommands.MoneyCMD;
 import GameCommands.PurchaseTimeBomb;
 import Objects.Command;
+import Objects.CommandGame;
+import Utils.GameUtils;
 import java.util.ArrayList;
 import java.util.List;
 import org.pircbotx.Colors;
@@ -21,61 +24,60 @@ import org.pircbotx.hooks.events.PrivateMessageEvent;
  * @author Stephen
  */
 public class GameListener extends ListenerAdapter{
-    List<Command> commandList = getCommandList();
+    List<CommandGame> commandList = getCommandList();
     
     
     @Override
     public void onMessage(MessageEvent event) throws Exception {
         String message = Colors.removeFormattingAndColors(event.getMessage());
         
-        if (message.startsWith(Global.commandPrefix)&&!message.matches("([ ]{0,}"+Global.commandPrefix+"{1,}[ ]{0,}){1,}")){
-            String command = message.toLowerCase().split(Global.commandPrefix)[1];
-            String[] cmdSplit = command.split(" ");
-            for (int i=0;i<commandList.size();i++){
-                if (commandList.get(i).commandTerms().contains(cmdSplit[0])){
-                    commandList.get(i).processCommand(event);
+        if (!GameUtils.areGamesBlocked(event.getChannel().getName())) {
+            if (message.startsWith(Global.commandPrefix)&&!message.matches("([ ]{0,}"+Global.commandPrefix+"{1,}[ ]{0,}){1,}")) {
+                String command = message.toLowerCase().split(Global.commandPrefix)[1];
+                String[] cmdSplit = command.split(" ");
+                for (int i=0;i<commandList.size();i++){
+                    if (commandList.get(i).commandTerms().contains(cmdSplit[0])) {
+                        commandList.get(i).processCommand(event);
+                    }
                 }
             }
+//            else if (message.toLowerCase().startsWith(Global.mainNick.toLowerCase()+", ")) {
+//                for (int i=0;i<commandList.size();i++){
+//                    if (commandList.get(i).isCommand(message)){
+//                        commandList.get(i).processCommand(event);
+//                    }
+//                }
+//            }
         }
-        else if (message.startsWith(Global.mainNick+", ")){
+    }
+    
+//    @Override
+//    public void onPrivateMessage(PrivateMessageEvent event) throws Exception {
+//        String message = Colors.removeFormattingAndColors(event.getMessage());
+//        
+//        if (message.startsWith(Global.commandPrefix)&&!message.matches("([ ]{0,}"+Global.commandPrefix+"{1,}[ ]{0,}){1,}")) {
 //            String command = message.split(Global.commandPrefix)[1];
 //            String[] cmdSplit = command.split(" ");
-            
-            for (int i=0;i<commandList.size();i++){
-                if (commandList.get(i).isCommand(message)){
-                    commandList.get(i).processCommand(event);
-                }
-            }
-        }
-    }
+//            for (int i=0;i<commandList.size();i++){
+//                if (commandList.get(i).commandTerms().contains(cmdSplit[0])) {
+//                    commandList.get(i).processCommand(event);
+//                }
+//            }
+//        }
+//        else if (message.toLowerCase().startsWith(Global.mainNick.toLowerCase())) {
+//            for (int i=0;i<commandList.size();i++){
+//                if (commandList.get(i).isCommand(message)){
+//                    commandList.get(i).processCommand(event);
+//                }
+//            }
+//        }
+//    }
     
-    @Override
-    public void onPrivateMessage(PrivateMessageEvent event) throws Exception {
-        String message = Colors.removeFormattingAndColors(event.getMessage());
-        
-        if (message.startsWith(Global.commandPrefix)&&!message.matches("([ ]{0,}"+Global.commandPrefix+"{1,}[ ]{0,}){1,}")){
-            String command = message.split(Global.commandPrefix)[1];
-            String[] cmdSplit = command.split(" ");
-            for (int i=0;i<commandList.size();i++){
-                if (commandList.get(i).commandTerms().contains(cmdSplit[0])){
-                    commandList.get(i).processCommand(event);
-                }
-            }
-        }
-        else if (message.toLowerCase().startsWith(Global.mainNick.toLowerCase())){
-            for (int i=0;i<commandList.size();i++){
-                if (commandList.get(i).isCommand(message)){
-                    commandList.get(i).processCommand(event);
-                }
-            }
-        }
-    }
-    
-    private List<Command> getCommandList() {
-        List<Command> listOfCommands = new ArrayList<>();
+    private List<CommandGame> getCommandList() {
+        List<CommandGame> listOfCommands = new ArrayList<>();
 //        listOfCommands.add(new MoneyCMD());
-//        listOfCommands.add(new PurchaseTimeBomb());
-        
+        listOfCommands.add(new PurchaseTimeBomb());
+        listOfCommands.add(new GameBombCMD());
         return(listOfCommands);
     }
     
