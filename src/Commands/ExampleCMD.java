c09326f452c21f5060df29ec5efa904138bcaaa1
AeroSteveO <thesteveo64@gmail.com>
2015-07-18 23:47:04 -0400

    changed up some formatting

--- src/Commands/ExampleCMD.java
+++ src/Commands/ExampleCMD.java
@@ -33,19 +33,20 @@ import org.pircbotx.hooks.Event;
  *          Command description here
  *
  */
-public class ExampleCMD implements Command{
+public class ExampleCMD implements Command {
+    
     @Override
-    public String toString(){
+    public String toString() {
         return("Some text about the command that will never be used");
     }
     
     @Override
-    public boolean isCommand(String toCheck){
+    public boolean isCommand(String toCheck) {
         return toCheck.equalsIgnoreCase(Global.mainNick+", phrase command check"); // Phrase that when spoken will activate the command
     }
     
     @Override
-    public ArrayList<String> commandTerms(){
+    public ArrayList<String> commandTerms() {
         ArrayList<String> a = new ArrayList<>();
         a.add("command"); // Terms that when prefixed by the command prefix, will activate the command
 // NOTE: these should be all lowercase
@@ -53,9 +54,9 @@ public class ExampleCMD implements Command{
     }
     
     @Override
-    public void processCommand(Event event){
+    public void processCommand(Event event) {
         
-        CommandMetaData data = new CommandMetaData(event,true);
+        CommandMetaData data = new CommandMetaData(event, true);
         
         String caller = data.getCaller(); // Nick of the user who called the command
         boolean isVerified = data.isVerifiedBotOwner(); // True if the user who called the command is the bot owner
