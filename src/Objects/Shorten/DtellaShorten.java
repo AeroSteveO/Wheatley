b5acecec5ccbf5bda4dfe6007f7bbfae50248d6f
AeroSteveO <thesteveo64@gmail.com>
2016-10-03 22:53:07 -0500

    removed unused imports

--- src/Objects/Shorten/DtellaShorten.java
+++ src/Objects/Shorten/DtellaShorten.java
@@ -6,12 +6,6 @@
 package Objects.Shorten;
 
 import Utils.TextUtils;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.pircbotx.Colors;
