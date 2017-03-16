9287b9cb2b47ddebb2ecca945e4471e215ca6080
AeroSteveO <thesteveo64@gmail.com>
2017-03-15 21:58:38 -0500

    i'm not sure why it grabbed the file without the changes to it...

--- src/Commands/Hashtagify.java
+++ src/Commands/Hashtagify.java
@@ -3,73 +3,74 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Commands;
 
 import Objects.Command;
+import Objects.CommandMetaData;
+import Objects.MapArray;
+import Wheatley.CommandListener;
 import Wheatley.Global;
 import java.util.ArrayList;
 import org.pircbotx.Colors;
 import org.pircbotx.hooks.Event;
-import org.pircbotx.hooks.events.MessageEvent;
-import org.pircbotx.hooks.events.PrivateMessageEvent;
 
 /**
  *
  * @author Stephen
- * 
- * Requirements:
- * - APIs
- *    N/A
- * - Custom Objects
- *    Command
- * - Utilities
- *    N/A
- * - Linked Classes
- *    N/A
- * 
- * Activate Command with:
- *      !PickAPort
- *          Responds with a valid port number for use in a program
- * 
+ *
+ * Requirements: - APIs N/A - Custom Objects Command - Utilities N/A - Linked
+ * Classes N/A
+ *
+ * Activate Command with: !PickAPort Responds with a valid port number for use
+ * in a program
+ *
  */
+public class Hashtagify implements Command {
 
-public class PickAPort implements Command{
-    
-    @Override
-    public String toString(){
-        return("PICKAPORT");
-    }
-    
-    @Override
-    public boolean isCommand(String toCheck){
-        return false;
-    }
-    
-    @Override
-    public ArrayList<String> help(String command) {
-        ArrayList<String> a = new ArrayList<>();
-        a.add(Colors.BOLD + Global.commandPrefix + "PickAPort" + Colors.NORMAL + ": Responds a valid port number to use in port forwarding");
-        return a;
-    }
+  @Override
+  public String toString() {
+    return ("hashtagify");
+  }
 
-    
-    @Override
-    public ArrayList<String> commandTerms(){
-        ArrayList<String> a = new ArrayList<>();
-        a.add("pickaport");
-        return a;
+  @Override
+  public boolean isCommand(String toCheck) {
+    return false;
+  }
+
+  @Override
+  public ArrayList<String> help(String command) {
+    ArrayList<String> a = new ArrayList<>();
+    a.add(Colors.BOLD + Global.commandPrefix + "Hashtagify" + Colors.NORMAL + ": Turns the previous line into an amazing hashtag");
+    return a;
+  }
+
+  @Override
+  public ArrayList<String> commandTerms() {
+    ArrayList<String> a = new ArrayList<>();
+    a.add("hashtagify");
+    return a;
+  }
+
+  @Override
+  public void processCommand(Event event) {
+    CommandMetaData commandData = new CommandMetaData(event, false);
+    String respondTo = commandData.respondToIgnoreMessage();
+    String channel = commandData.getCommandChannel();
+    ArrayList<ArrayList<String>> logCopy = CommandListener.logger.getArray(channel);
+    if (CommandListener.logger.isEmpty(channel)) {
+      return;
     }
-    
-    @Override
-    public void processCommand(Event event){
-        if (event instanceof MessageEvent){
-            MessageEvent mEvent = (MessageEvent) event;
-            mEvent.getBot().sendIRC().message(mEvent.getChannel().getName(),Colors.BOLD+"Port Number: "+Colors.NORMAL+(int) (1025+(Math.random()*(65534-1025+1))));
-        }
-        else   if (event instanceof PrivateMessageEvent){
-            PrivateMessageEvent mEvent = (PrivateMessageEvent) event;
-            mEvent.getBot().sendIRC().message(mEvent.getUser().getNick(),Colors.BOLD+"Port Number: "+Colors.NORMAL+(int) (1025+(Math.random()*(65534-1025+1))));
-        }
+    String line = logCopy.get(logCopy.size() - 2).get(1);
+    String[] lineSplit = line.split(" ");
+    String hashtag = "#";
+    for (String item : lineSplit) {
+      hashtag += capFirst(item.trim());
     }
+    event.getBot().sendIRC().message(respondTo, hashtag);
+  }
+
+  String capFirst(String name) {
+    name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    return name;
+  }
 }
