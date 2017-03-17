0e6839d2adc64047035c5392c78015d0b8833b90
AeroSteveO <thesteveo64@gmail.com>
2017-03-17 16:35:15 -0500

    space command

--- src/Commands/SpaceCMD.java
+++ src/Commands/SpaceCMD.java
@@ -3,73 +3,65 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Commands;
 
 import Objects.Command;
+import Objects.CommandMetaData;
 import Wheatley.Global;
 import java.util.ArrayList;
 import org.pircbotx.Colors;
 import org.pircbotx.hooks.Event;
-import org.pircbotx.hooks.events.MessageEvent;
-import org.pircbotx.hooks.events.PrivateMessageEvent;
 
 /**
  *
  * @author Stephen
- * 
- * Requirements:
- * - APIs
- *    N/A
- * - Custom Objects
- *    Command
- * - Utilities
- *    N/A
- * - Linked Classes
- *    N/A
- * 
- * Activate Command with:
- *      !PickAPort
- *          Responds with a valid port number for use in a program
- * 
+ *
+ * Activate Command with: !SPACE Responds with a space core quote from portal 2
+ *
  */
+public class SpaceCMD implements Command {
 
-public class PickAPort implements Command{
-    
-    @Override
-    public String toString(){
-        return("PICKAPORT");
-    }
-    
-    @Override
-    public boolean isCommand(String toCheck){
-        return false;
-    }
-    
-    @Override
-    public ArrayList<String> help(String command) {
-        ArrayList<String> a = new ArrayList<>();
-        a.add(Colors.BOLD + Global.commandPrefix + "PickAPort" + Colors.NORMAL + ": Responds a valid port number to use in port forwarding");
-        return a;
-    }
+  @Override
+  public String toString() {
+    return ("SPACE");
+  }
 
-    
-    @Override
-    public ArrayList<String> commandTerms(){
-        ArrayList<String> a = new ArrayList<>();
-        a.add("pickaport");
-        return a;
+  @Override
+  public boolean isCommand(String toCheck) {
+    return false;
+  }
+
+  @Override
+  public ArrayList<String> help(String command) {
+    ArrayList<String> a = new ArrayList<>();
+    a.add(Colors.BOLD + Global.commandPrefix + "Space" + Colors.NORMAL + ": SPAAAAAAAAAAAAAAAAAAAACE");
+    return a;
+  }
+
+  @Override
+  public ArrayList<String> commandTerms() {
+    ArrayList<String> a = new ArrayList<>();
+    a.add("space");
+    return a;
+  }
+
+  @Override
+  public void processCommand(Event event) {
+    CommandMetaData data = new CommandMetaData(event, false);
+    String respondTo = data.respondToIgnoreMessage();
+    switch ((int) (Math.random() * 3 + 1)) {
+      case 1:
+        event.getBot().sendIRC().message(respondTo, "What's your favorite thing about space? Mine is space.");
+        break;
+      case 2:
+        event.getBot().sendIRC().message(respondTo, "Ohmygodohmygodohmygod! I'm in space!");
+        break;
+      case 3:
+        String chan = data.getEventChannel();
+        event.getBot().sendIRC().message(respondTo, "Oh oh oh. This is space! I'm in space!");
+        event.getBot().sendIRC().message(respondTo, "So much space. Need to see it all.");
+        event.getBot().sendRaw().rawLine("KICK " + chan + " " + data.getCaller() + " Space Court. For people in space. Judge space sun presiding. Bam. Guilty. Of being in space.");
+        break;
     }
-    
-    @Override
-    public void processCommand(Event event){
-        if (event instanceof MessageEvent){
-            MessageEvent mEvent = (MessageEvent) event;
-            mEvent.getBot().sendIRC().message(mEvent.getChannel().getName(),Colors.BOLD+"Port Number: "+Colors.NORMAL+(int) (1025+(Math.random()*(65534-1025+1))));
-        }
-        else   if (event instanceof PrivateMessageEvent){
-            PrivateMessageEvent mEvent = (PrivateMessageEvent) event;
-            mEvent.getBot().sendIRC().message(mEvent.getUser().getNick(),Colors.BOLD+"Port Number: "+Colors.NORMAL+(int) (1025+(Math.random()*(65534-1025+1))));
-        }
-    }
+  }
 }
