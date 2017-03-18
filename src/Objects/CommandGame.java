64fedd55e6dbb5b321c295652e9ab2e308d4aae2
AeroSteveO <thesteveo64@gmail.com>
2017-03-18 12:01:27 -0500

    cleaning and commenting

--- src/Objects/CommandGame.java
+++ src/Objects/CommandGame.java
@@ -25,42 +25,56 @@ import org.pircbotx.hooks.events.MessageEvent;
 public interface CommandGame  {
     
     /**
-     *
+     * Determines if this object contains a GAME or a game COMMAND.
+     * Game commands can include general commands related to them.
+     * 
+     * <ul>
+     * <li>Games would include stuff like hangman
+     * <li>Non-games would include stuff like money, cheat, and give
+     * </ul>
+     * 
      * @return boolean true if the object implementing the interface is a game
      * and false if it is a general command
      * 
-     * -Games would include stuff like hangman
-     * -Non-games would include stuff like money, cheat, and give
      */
     boolean isGame();
     
     /**
-     *
+     * Determines if the game in this object is "short" or not. Single line
+     * games are considered short, and multi line interactive games are considered
+     * long.
+     * <ul>
+     * <li>Single line games are those that only require the bot to output a single
+     *  line (Think lotto, omgword, and bomb)
+     * <li>Long games may require constant interaction between the user and the bot
+     *  (Think hangman, and mastermind)
+     * </ul>
+     * 
      * @return boolean true if the game is a multi-line, "Long" game, and false
      * if the game is a single line "Short" game
      * 
-     * -Single line games are those that only require the bot to output a single
-     *  line (Think lotto, omgword, and bomb)
-     * -Long games may require constant interaction between the user and the bot
-     *  (Think hangman, and mastermind)
      */
     boolean isShortGame();
     
     /**
-     *
+     * Processes the provided message event for the command.
+     * 
      * @param event the event that triggered the game/command to be run, allows
      * the command to use the full data from the event.
      */
     public void processCommand(MessageEvent event);
     
     /**
-     *
+     * Returns a list of command terms that can be processed by this object.
+     * 
      * @return ArrayList String of terms that will activate the command
      */
     public ArrayList<String> commandTerms();
     
     /**
-     *
+     * Determines if the input string is a command term that can be processed
+     * by this object.
+     * 
      * @param toCheck
      * @return boolean true if the string is an activating command, false if the
      * string is not to activate the game/command
--- src/Objects/SimpleSettings.java
+++ src/Objects/SimpleSettings.java
@@ -62,21 +62,41 @@ public class SimpleSettings extends SettingsBase {
 //    Map<String, String> stuff = new TreeMap<String, String>();
 //    File file = new File("doNotSave");
 //    Map <String, Object> settings = Collections.synchronizedMap(new TreeMap<String, Object>( ));
-    
+
+  /**
+   * Constructs the settings using the input FILE for the backing JSON.
+   * @param file File to store the JSON settings in.
+   */
     public SimpleSettings(File file){
         super(file);
     }
-    public SimpleSettings(String filename){
+
+  /**
+   * Basic constructor to create the settings object. Requires a filename for
+   * the backing file to the settings.
+   * @param filename Name of the file to save the JSON settings to.
+   */
+  public SimpleSettings(String filename){
         super(filename);
     }
     
-    public void create(String key, String value){
+  /**
+   * Creates the input key:value pair in the settings file
+   * @param key key to use to locate the setting by
+   * @param value value to set the setting item to
+   */
+  public void create(String key, String value){
         key=key.toLowerCase();
         if (!settings.containsKey(key))
             settings.put(key,value);
     }
     
-    public void create(String key, ArrayList<String> value){
+  /**
+   * Creates the list setting located by the key to the input list value
+   * @param key key to locate the list by
+   * @param value list to set in the settings
+   */
+  public void create(String key, ArrayList<String> value){
         key=key.toLowerCase();
         if (!key.endsWith("list"))
             key += "list";
@@ -99,7 +119,13 @@ public class SimpleSettings extends SettingsBase {
             throw new UnsupportedOperationException("KEY MISSING");
     }
     
-    public ArrayList<String> getArray(String key){
+  /**
+   * Gets an array located under the input key from the settings file.
+   * 
+   * @param key Key used to identify the array
+   * @return List of strings from the settings
+   */
+  public ArrayList<String> getArray(String key){
         key=key.toLowerCase();
         if (!key.endsWith("list"))
             key += "list";
@@ -109,7 +135,12 @@ public class SimpleSettings extends SettingsBase {
             throw new UnsupportedOperationException("KEY MISSING");
     }
     
-    public void setString(String key, String value){
+  /**
+   * Sets the string setting located by the input key to the input value.
+   * @param key Key to locate the setting by
+   * @param value Value to set the setting to
+   */
+  public void setString(String key, String value){
         key=key.toLowerCase();
         
         if(settings.containsKey(key))
@@ -118,7 +149,12 @@ public class SimpleSettings extends SettingsBase {
             throw new UnsupportedOperationException("KEY MISSING");
     }
     
-    public void setArray(String key, ArrayList<String> value){
+  /**
+   * Sets the array setting located by the input key to the input value.
+   * @param key Key to locate the setting by
+   * @param value Value to set the setting to
+   */
+  public void setArray(String key, ArrayList<String> value){
         key=key.toLowerCase();
         if (!key.endsWith("list"))
             key += "list";
