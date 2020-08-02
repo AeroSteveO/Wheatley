62267d61f42755ebb89d463dec2b7f35f0b7e0c4
Stephen <thesteveo64@gmail.com>
2020-08-02 16:02:34 -0500

    re-enable blarghlebot commands

--- src/rapternet/irc/bots/wheatley/listeners/WheatleyMain.java
+++ src/rapternet/irc/bots/wheatley/listeners/WheatleyMain.java
@@ -155,7 +155,7 @@ public class WheatleyMain extends ListenerAdapter {
                     .setMaxLineLength(425)
                     .setListenerManager(BackgroundListener)//Allow for logger background listener
                     .addListener(new WheatleyMain())       //This main class's listener
-//                    .addListener(new Blarghlebot())        //Trollbot Listener
+                    .addListener(new Blarghlebot())        //Trollbot Listener
                     .addListener(new Swapper())
                     .addListener(new GameOmgword())        //omgword game listener
                     .addListener(new GameReverse())        //reverse the word game
